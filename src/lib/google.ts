88b778f6a9303bc0c8bb57ac261780427c70d05e
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-01-03 19:14:44 -0300

    feat: create google events

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -1,39 +1,46 @@
-import dayjs from "dayjs";
-import { google } from "googleapis";
-import { prisma } from "./prisma";
+import dayjs from 'dayjs'
+import { google } from 'googleapis'
+import { prisma } from './prisma'
 
-export async function getGoogleOAuthToken(userId: string){
+export async function getGoogleOAuthToken(userId: string) {
   const account = await prisma.account.findFirstOrThrow({
     where: {
       provider: 'google',
-      user_id: userId
-    }
+      user_id: userId,
+    },
   })
 
   const auth = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET
+    process.env.GOOGLE_CLIENT_SECRET,
   )
 
   auth.setCredentials({
     access_token: account.access_token,
     refresh_token: account.refresh_token,
-    expiry_date: account.expires_at ? account.expires_at * 1000 : null
+    expiry_date: account.expires_at ? account.expires_at * 1000 : null,
   })
 
-  if(!account.expires_at) {
+  if (!account.expires_at) {
     return auth
   }
 
   const isTokenExpired = dayjs(account.expires_at * 1000).isBefore(new Date())
 
-  if(isTokenExpired) {
+  if (isTokenExpired) {
     const { credentials } = await auth.refreshAccessToken()
-    const { access_token, expiry_date, id_token, refresh_token, scope, token_type } = credentials
+    const {
+      access_token,
+      expiry_date,
+      id_token,
+      refresh_token,
+      scope,
+      token_type,
+    } = credentials
 
     await prisma.account.update({
       where: {
-        id: account.id
+        id: account.id,
       },
       data: {
         access_token,
@@ -41,16 +48,15 @@ export async function getGoogleOAuthToken(userId: string){
         id_token,
         refresh_token,
         scope,
-        token_type
-      }
+        token_type,
+      },
     })
 
     auth.setCredentials({
       access_token,
       refresh_token,
-      expiry_date
+      expiry_date,
     })
-
   }
 
   return auth
